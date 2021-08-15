f6547bf4ee539fbd1dc30ab27c8f353f529dc111
alexalins <alexa.glinsoo@gmail.com>
2021-08-15 16:58:12 -0300

    add input e button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import Input from './Input';
 
 class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        text: ''
     };
   }
 
+  onChangeText(text) {
+      this.setState({
+        text
+      });
+  }
+
+  onPress() {
+    console.log(this.state.text)
+  }
+
   render() {
+    const { text } = this.state;
     return (
-      <View>
-        <View>
-            <Input />
+      <View style={style.formContainer}>
+        <View  style={style.inputContainer}>
+            <Input onChangeText={text => this.onChangeText(text)} value={text}/>
+        </View>
+        <View  style={style.buttonContainer}>
+            <Button title='add' onPress={() => this.onPress()}/>
         </View>
       </View>
     );
@@ -21,3 +36,15 @@ class Form extends Component {
 }
 
 export default Form;
+
+const style = StyleSheet.create({
+    formContainer: {
+        flexDirection: 'row'
+    },
+    inputContainer: {
+        flex: 4
+    },
+    buttonContainer: {
+        flex: 1
+    }
+});
